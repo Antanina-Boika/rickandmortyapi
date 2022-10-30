61f54f69d79418bf9523f5bdecf65ef340f5b6a7
«Antanina_Boika» <antanina.boika@gmail.com>
2022-10-31 02:49:15 +0300

    Add pagination

--- index.js
+++ index.js
@@ -2,10 +2,166 @@ const url = "https://rickandmortyapi.com/api/character/";
 const appContainer = document.querySelector(".app_container");
 const modalContainer = document.querySelector(".modal_container");
 let page = null;
+let activePage = null;
 
+//implementation with pagination
+function fetchCharacters(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let ul = document.querySelector(".pl");
+      for (let i = 1; i <= data.info.pages; i++) {
+        let li = document.createElement("li");
+        li.classList.add("list_style");
+        li.setAttribute("id", i);
+        li.innerHTML = i;
+        ul.appendChild(li);
+      }
+      activePage = ul.firstElementChild;
+      activePage.classList.add("act");
+      fetch(`${url}?page=${1}`)
+        .then((response) => response.json())
+        .then((data) => {
+          for (let i = 0; i < data.results.length; i++) {
+            makeGallery(data.results[i]);
+          }
+        })
+        .catch((error) => console.error(error));
+    })
+    .catch((error) => console.error(error));
+}
 
+//function of rendering information from the server
+function makeGallery(info) {
+  let characterContainer = document.createElement("div");
+  characterContainer.classList.add("character_container");
+  characterContainer.setAttribute("id", info.id);
+  document.querySelector(".app_container").appendChild(characterContainer);
+  let characterImg = document.createElement("img");
+  characterImg.classList.add("character_img");
+  characterImg.setAttribute("src", info.image);
+  characterContainer.appendChild(characterImg);
+  let characterName = document.createElement("p");
+  characterName.classList.add("character_name_prev");
+  characterName.innerHTML = info.name;
+  characterContainer.appendChild(characterName);
+}
+
+fetchCharacters(url);
+
+//pagination
+document.querySelector(".pl").addEventListener("click", function (event) {
+  if (event.target.classList.contains("list_style")) {
+    event.preventDefault();
+    event.stopPropagation();
+    let id = event.target.innerHTML;
+    activePage.classList.remove("act");
+    activePage = event.target;
+    appContainer.innerHTML = "";
+    fetch(`${url}?page=${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        for (let i = 0; i < data.results.length; i++) {
+          makeGallery(data.results[i]);
+        }
+      })
+      .catch((error) => console.error(error));
+    activePage.classList.add("act");
+  }
+});
+
+appContainer.addEventListener("click", function (event) {
+  if (
+    event.target.classList.contains("character_container") ||
+    event.target.classList.contains("character_img") ||
+    event.target.classList.contains("character_name_prev")
+  ) {
+    event.preventDefault();
+    event.stopPropagation();
+    let id = null;
+    if (event.target.classList.contains("character_container")) {
+      id = event.target.getAttribute("id");
+    }
+    if (
+      event.target.classList.contains("character_img") ||
+      event.target.classList.contains("character_name_prev")
+    ) {
+      id = event.target.parentNode.getAttribute("id");
+    }
+    fetch(url + id)
+      .then((response) => response.json())
+      .then((data) => {
+        appContainer.classList.toggle("display_none");
+        document.querySelector(".pl").classList.toggle("display_none");
+        modalContainer.classList.toggle("display_none");
+        modalContainer.innerHTML = `
+        <div class="modal_character"> 
+          <div class="cl_btn"></div>       
+          <img class="character_full_img"/>
+          <div class="character_info">
+            <div>      
+              <p class="character_info_title">Name:</p>
+              <p class="character_info_parag character_name"></p>  
+              <p class="character_info_title">Gender:</p>
+              <p class="character_info_parag character_gender"></p>
+              <p class="character_info_title">Status:</p>
+              <p class="character_info_parag character_status"></p></div>    
+            <div>
+              <p class="character_info_title">Type:</p>
+              <p class="character_info_parag character_type"></p>
+              <p class="character_info_title">Species:</p>
+              <p class="character_info_parag character_species"></p>
+              <p class="character_info_title">First appearance:</p>
+              <p class="character_info_parag character_appearance"></p>                    
+            </div>
+          </div>        
+      </div>`;
+        document
+          .querySelector(".character_full_img")
+          .setAttribute("src", data.image);
+        document.querySelector(".character_name").innerHTML = data.name;
+        document.querySelector(".character_status").innerHTML = data.status;
+        document.querySelector(".character_species").innerHTML = data.species;
+        document.querySelector(".character_type").innerHTML = data.type;
+        document.querySelector(".character_appearance").innerHTML =
+          data.episode[0].split("episode/")[1];
+        document.querySelector(".character_gender").innerHTML = data.gender;
+        document
+          .querySelector(".cl_btn")
+          .addEventListener("click", function () {
+            appContainer.classList.toggle("display_none");
+            modalContainer.classList.toggle("display_none");
+            document.querySelector(".pl").classList.toggle("display_none");
+          });
+      })
+      .catch((error) => console.error(error));
+  }
+});
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+//scroll animation to the top of the page
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector(".btn_to_top").style.cssText = `display: block`;
+    document
+      .querySelector(".btn_to_top")
+      .addEventListener("click", topFunction);
+  } else {
+    document.querySelector(".btn_to_top").style.cssText = `display:none;`;
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//implementation with infinite loading
 //a function that will determine when it's time to send a new request.
-function checkPosition() {
+/*function checkPosition() {
   const height = document.body.offsetHeight;
   const screenHeight = window.innerHeight;
   const scrolled = window.scrollY;
@@ -149,14 +305,16 @@ window.onscroll = function () {
 //scroll animation to the top of the page
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.querySelector('.btn_to_top').style.cssText =`display: block`;
-    document.querySelector('.btn_to_top').addEventListener("click", topFunction);
+    document.querySelector(".btn_to_top").style.cssText = `display: block`;
+    document
+      .querySelector(".btn_to_top")
+      .addEventListener("click", topFunction);
   } else {
-   document.querySelector('.btn_to_top').style.cssText =`display:none;` 
+    document.querySelector(".btn_to_top").style.cssText = `display:none;`;
   }
 }
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-}
+}*/
